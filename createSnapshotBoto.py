9eed4a49c3c4f6f6a3c16ddd165c8e67c3590fab
Yash_Bhatt <yashbhatt1304@gmail.com>
2024-06-10 19:30:50 +0530

    Task 1 Creating Snapshot using Boto3

---
+++ createSnapshotBoto.py
@@ -0,0 +1,37 @@
+import boto3
+ec2 = boto3.client('ec2')
+
+#Creating Snapshot using create_snapshots
+#It uses InstanceId as mandatory filed
+
+response = ec2.create_snapshots(
+        Description="Snapshot of EC2 instance",
+        InstanceSpecification={
+            'InstanceId': "i-0e32a8fe1e894fdab",
+            'ExcludeBootVolume': False
+        },
+        TagSpecifications=[
+        {
+            'ResourceType': 'snapshot',
+            'Tags': [
+                {
+                    'Key': "Team",
+                    'Value': "Group A"
+                },
+            ]
+        },
+        ]
+)
+print(response)
+    
+
+#Creating Snapshot using create_snapshot
+#It uses VolumeId as mandatory filed
+
+# response = ec2.create_snapshot(
+#     Description='This is my root volume snapshot.',
+#     VolumeId='vol-0a83e06d0331af24a',
+# )
+
+# print(response)
+
